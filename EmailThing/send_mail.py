ccdb32bc73f759df1f9342eacf607434f42fec83
Rick Segal <rick.segal@gmail.com>
2021-08-22 22:32:59 -0700

    First for 30day

---
+++ EmailThing/send_mail.py
@@ -0,0 +1,33 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+
+username = 'rick.segal@gmail.com'
+password = 'bvxhafdudnxluotm'
+
+
+def send_mail(text='Email body',subject='Hello World', from_email='Rick <rick.segal@gmail.com>', to_emails=None, html=None):
+    assert isinstance(to_emails, list)
+    msg = MIMEMultipart('alternative')
+    msg['From'] = from_email
+    msg['To'] = ", ".join(to_emails)
+    msg['Subject'] = subject
+
+    txt_part = MIMEText(text, 'plain')
+    msg.attach(txt_part)
+    if html != None:
+        html_part = MIMEText(html,'html')
+        msg.attach(html_part)
+
+    msg_str = msg.as_string()
+    # login to my smtp server
+    server = smtplib.SMTP(host='smtp.gmail.com', port=587)
+    server.ehlo()
+    server.starttls()
+    server.login(username,password)
+    server.sendmail(from_email, to_emails, msg_str)
+
+    server.quit()
+
+send_mail(to_emails=['rick.segal@gmail.com', 'rick@hellosimple.com'])
