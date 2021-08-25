cabdfbf1b31fcda9cc36e6b6fa406ddaa193ddc4
Rick Segal <rick.segal@gmail.com>
2021-08-24 20:51:40 -0700

    Ding what I do

---
+++ Corey_Stuff/script.py
@@ -0,0 +1,16 @@
+# This is a file for the lesson
+import sys
+
+print(sys.version)
+print(sys.executable)
+
+
+def greet(who_to_greet):
+    greeting = 'Hello, {}'.format(who_to_greet)
+    return greeting
+
+
+print(greet('World'))
+print(greet('Rick'))
+
+# Brought to you by schmo
